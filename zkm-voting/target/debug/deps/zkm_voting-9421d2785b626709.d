bf294f252677e375244b613b41ca1e3273899d3e
gil7788 <gil7788@gmail.com>
2025-06-03 20:31:48 +0300

    Init Cargo Prover/Verifier Directory

---
+++ zkm-voting/target/debug/deps/zkm_voting-9421d2785b626709.d
@@ -0,0 +1,5 @@
+/home/user/Projects/hackatons/belgrade-2025/voting-pvm-zkvm/zkm-voting/target/debug/deps/zkm_voting-9421d2785b626709: src/main.rs
+
+/home/user/Projects/hackatons/belgrade-2025/voting-pvm-zkvm/zkm-voting/target/debug/deps/zkm_voting-9421d2785b626709.d: src/main.rs
+
+src/main.rs:
---
+++ zkm-voting/target/debug/zkm-voting.d
@@ -0,0 +1,1 @@
+/home/user/Projects/hackatons/belgrade-2025/voting-pvm-zkvm/zkm-voting/target/debug/zkm-voting: /home/user/Projects/hackatons/belgrade-2025/voting-pvm-zkvm/zkm-voting/src/main.rs
